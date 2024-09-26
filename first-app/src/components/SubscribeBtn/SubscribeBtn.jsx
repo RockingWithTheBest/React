351b40b725c5c4456795e61a88c88e7deabe3afd
RockingWithTheBest <sikanweclyde@gmail.com>
2024-09-26 23:28:43 +0300

    added component subscribe button

---
+++ first-app/src/components/SubscribeBtn/SubscribeBtn.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styling.css';
+
+class SubscribeBtn extends React.Component {
+    render() {
+      return (
+        <button className = "subbutton">
+               Subscribe
+          </button>
+      )
+    }
+  }
+
+  export default SubscribeBtn;
