7d43e6c0f2323150d841c8d9aadec9025870fc9e
RockingWithTheBest <sikanweclyde@gmail.com>
2024-09-26 23:52:33 +0300

    app App.jsx added and edited

--- first-app/src/App.jsx
+++ first-app/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import FirstDescription from './components/FirstDescription/FirstDescription';
+import FirstSearchPhoto from './components/FirstSearchPhoto/FirstSearchPhoto';
+import Footer from './components/Footer/Footer';
+import LivingRoomPhoto from './components/LivingRoomPhoto/LivingRoomPhoto';
+import FamilyPriority from './components/FamilyPriority/FamilyPriority';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+export default class App extends React.Component {
+  render() {
+    return (
+    <div>
+      <div className="WrapperOneThird">
+          <div className = "detail">
+              {/* <h1>{this.props.type} Component</h1> */}
+              <FirstDescription /> 
+              <FirstSearchPhoto />
+          </div>  
+          <LivingRoomPhoto/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <FamilyPriority /> 
+      <Footer/>
+    </div>
+      
+    )
+  }
 }
-
-export default App
