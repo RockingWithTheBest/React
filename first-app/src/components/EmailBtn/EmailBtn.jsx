cbb99a2c22140b55abbdfde76689b06f5a3f1f1b
RockingWithTheBest <sikanweclyde@gmail.com>
2024-09-26 23:16:02 +0300

    added email component

---
+++ first-app/src/components/EmailBtn/EmailBtn.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import './styling.css';
+
+
+class EmailBtn extends React.Component {
+    render() {
+      return (
+        <button className="email">
+               Enter your email
+          </button>
+      )
+    }
+}
+
+export default EmailBtn;
