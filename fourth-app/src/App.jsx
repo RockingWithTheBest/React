eb03eab76e4710ff9010b66f7ad51be0339a922c
RockingWithTheBest <sikanweclyde@gmail.com>
2024-10-08 17:52:55 +0300

    feat : added a new Router called '/edit/: id' which helps with the movement from the Main table to the update page

--- fourth-app/src/App.jsx
+++ fourth-app/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import DisplayItems from './components/displayItems'
 import { BrowserRouter, Routes, Route } from'react-router-dom'
 import Create from './components/Create/Create'
+import Update from './components/Update/Update'
 function App() {
 
 
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path = "/" element ={<DisplayItems/>}/>
       <Route path = "/create" element ={<Create/>}/>
+      <Route path = "/edit/:id" element ={<Update/>}/>
     </Routes>
     </BrowserRouter>
   )
