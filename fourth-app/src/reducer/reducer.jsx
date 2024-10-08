7d148175190dd7a3020041d88f20a7c20998b597
RockingWithTheBest <sikanweclyde@gmail.com>
2024-10-08 20:03:23 +0300

    feat : added  a delete state to the reducer

--- fourth-app/src/reducer/reducer.jsx
+++ fourth-app/src/reducer/reducer.jsx
@@ -6,6 +6,8 @@ const useSlice = createSlice({
     initialState: Ecomerce,
     reducers: {
         addProduct: (state, action)=>{
+            console.log(action);
+            console.log(state);
             state.push(action.payload)
         },
         updateProduct: (state, action)=>{
@@ -18,9 +20,16 @@ const useSlice = createSlice({
                 productUpdated.brand = brand;
                 productUpdated.Remainder = Remainder;
             }
+        },
+        deleteProduct: (state, action)=>{
+            const {id } = action.payload;
+            const productDelete = state.find(product =>product.id == id);
+            if(productDelete){
+                return state.filter(f => f.id !== id);
+            }
         }
     }
 })
 
-export const {addProduct, updateProduct} = useSlice.actions;
-export default useSlice.reducer;
+export const {addProduct, updateProduct, deleteProduct} = useSlice.actions;
+export  const UserReducer = useSlice.reducer;
